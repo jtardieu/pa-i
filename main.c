ded43af3895404ad136e3886758cfcefb3810691
Julie Tardieu <jtardieu@k1r1p8.42mulhouse.fr>
2025-11-19 16:47:38 +0100

    il me fon chier eux la

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 14:23:28 by jtardieu          #+#    #+#             */
-/*   Updated: 2025/11/18 18:07:50 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/19 15:55:58 by jtardieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@
 #include <limits.h>
 #include <stdio.h>
 
-void	affiche(int i, ...);
-
 int	main(int ac, char **av)
 {
 	(void) ac;
-	//affiche(atoi(av[3]), *av[1], *av[2]);
 	char a = av[1][0];
 	char *b = av[2];
 	int c = ft_atoi(av[3]);
@@ -49,149 +46,88 @@ int	main(int ac, char **av)
 	printf(" %c %c %c \n", '2', '1', 0);
 	printf(" %c %c %c \n\n", 0, '1', '2');
 
-	
 	ft_printf("ft\n");
 	ft_printf(" %d \n", 0);
 	ft_printf(" %d \n", -1);
-	ft_printf(" %d \n", 1);
-	ft_printf(" %d \n", 9);
 	ft_printf(" %d \n", 10);
 	ft_printf(" %d \n", 11);
-	ft_printf(" %d \n", 15);
-	ft_printf(" %d \n", 16);
-	ft_printf(" %d \n", 17);
-	ft_printf(" %d \n", 99);
 	ft_printf(" %d \n", 100);
-	ft_printf(" %d \n", 101);
-	ft_printf(" %d \n", -9);
-	ft_printf(" %d \n", -10);
-	ft_printf(" %d \n", -11);
-	ft_printf(" %d \n", -14);
-	ft_printf(" %d \n", -15);
-	ft_printf(" %d \n", -16);
-	ft_printf(" %d \n", -99);
 	ft_printf(" %d \n", -100);
-	ft_printf(" %d \n", -101);
-	ft_printf(" %d \n", INT_MAX);
-	ft_printf(" %d \n", INT_MIN);
-	ft_printf(" %d \n", LONG_MAX);
-	ft_printf(" %d \n", LONG_MIN);
-	ft_printf(" %d \n", UINT_MAX);
-	ft_printf(" %d \n", ULONG_MAX);
-	ft_printf(" %d \n", 9223372036854775807LL);
-	ft_printf(" %d %d %d %d %d %d %d", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+	e=ft_printf(" %d .\n", INT_MIN);
+		ft_printf("char %d\n",e);
 
 	printf("\n\nprint\n");
 	printf(" %d \n", 0);
 	printf(" %d \n", -1);
 	printf(" %d \n", 1);
-	printf(" %d \n", 9);
 	printf(" %d \n", 10);
 	printf(" %d \n", 11);
-	printf(" %d \n", 15);
-	printf(" %d \n", 16);
-	printf(" %d \n", 17);
-	printf(" %d \n", 99);
 	printf(" %d \n", 100);
-	printf(" %d \n", 101);
-	printf(" %d \n", -9);
-	printf(" %d \n", -10);
-	printf(" %d \n", -11);
-	printf(" %d \n", -14);
-	printf(" %d \n", -15);
-	printf(" %d \n", -16);
-	printf(" %d \n", -99);
 	printf(" %d \n", -100);
-	printf(" %d \n", -101);
-	printf(" %d \n", INT_MAX);
-	printf(" %d \n", INT_MIN);
-	printf(" %d \n", LONG_MAX);
-	printf(" %d \n", LONG_MIN);
-	printf(" %d \n", UINT_MAX);
-	printf(" %d \n", ULONG_MAX);
-	printf(" %d \n", 9223372036854775807LL);
-	printf(" %d %d %d %d %d %d %d \n\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+	e=printf(" %d .\n", INT_MIN);
+	ft_printf("char %d\n\n",e);
 
-	ft_printf("ft Char: %c\n", a);
-    ft_printf("ft String: %s\n", b);
-    ft_printf("ft Pointer: %p\n", (void *)0x1234abcd);
-    ft_printf("ft Decimal: %d\n", c);
-    ft_printf("ft Integer: %i\n", 1337);
-    ft_printf("ft Unsigned: %u\n", 3000000000U);
-    ft_printf("ft Hex lower: %x\n", -900000000);
-    ft_printf("ft Hex upper: %X\n", 48879);
-    ft_printf("ft Percent: %%\n");
-	ft_printf("ft une bite de moine %c\n hun hun %s\n chifre %d\n\n",a,b,c);
+	e = ft_printf("%c\t", a);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%s\t", b);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%p\t", NULL);
+    ft_printf("char %d\n",e);
+	e = ft_printf("%d\t", c);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%i\t", 1337);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%u\t", 3000000000U);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%x\t", 0);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%X\t", 48879);
+	ft_printf("char %d\n",e);
+    e = ft_printf("%%\t");
+	ft_printf("char %d\n",e);
+	e = ft_printf("ft une bite de moine %c\n hun hun %s\n chifre %d\n",a,b,c);
+	ft_printf("char %d\n\n",e);
 
 	printf("Char: %c\n", a);
     printf("String: %s\n", b);
-    printf("Pointer: %p\n", (void *)0x1234abcd);
-    printf("Decimal: %d\n", c);
+    printf("Pointer: %p\n", NULL);
+    printf("Decimal: %d\n", INT_MIN);
     printf("Integer: %i\n", 1337);
     printf("Unsigned: %u\n", 3000000000U);
-    printf("Hex lower: %x\n", -900000000);
-    printf("Hex upper: %X\n", 48879);
+    e=printf("%x\n", 0);
+	ft_printf("char %d\n\n",e);
+	printf("Hex upper: %X\n", 48879);
     printf("Percent: %%\n");
 	printf("chien d'home %c\n la suite %s\nchifre %d\n",a,b,c);
 	return 0;
 }
 
-void	affiche(int i, ...)
+
+int	cfekoi(char c, va_list var)
 {
-	int		j = 0;
-	char	argu ;
-	va_list	infoparam;
-	va_start (infoparam, i);
-	while (j != i)
+	size_t	i;
+
+	if (c == 'c')
+		ft_putchar_fd(va_arg(var, int), 1);
+	else if (c == 's')
+		return (ft_putstr_fd(va_arg(var, char *), 1) - 1);
+	else if (c == 'x')
+		return (inhexa(va_arg(var, unsigned int), 0) - 1);
+	else if (c == 'X')
+		return (inhexa(va_arg(var, unsigned int), 1) - 1);
+	else if (c == 'u')
+		return (inhexa(va_arg(var, unsigned int), 2) - 1);
+	else if (c == 'p')
 	{
-		argu = (char )va_arg(infoparam, int);
-		printf("j imagine space c ca %c\n",argu);
-		j++;
+		i = va_arg(var, size_t);
+		if (!i)
+			return (write(1, "(nil)", 5) - 1);
+		else
+			return ((write(1, "0x", 2) - 1) + inhexa(i, 0));
 	}
-	va_end(infoparam);
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr_fd(va_arg(var, int), 1));
+	else
+		write(1, &c, 1);
+	return (0);
 }
-
-// #include <stdarg.h>
-// #include <stdbool.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int maximum( int first, ... ) {
-
-//     int max = first;
-//     va_list parametersInfos;
-//     /* Initialize the va_list structure */
-//     va_start( parametersInfos, first );
-
-//     while( true ) {
-
-//         /* Extraction of the next integer */
-//         int current = (int) va_arg( parametersInfos, int );
-
-//         if ( current == -1 ) break;
-
-//         if ( current > max ) {
-//             max = current;
-//         }
-
-//     }
-
-//     /* Release va_list resources */
-//     va_end( parametersInfos );
-
-//     return max;
-// }
-
-// int main( int argc, char * argv[] ) {
-
-//     int result = maximum( 2, 11, 5, -1 );
-//     printf( "maximum( 2, 11, 5, -1 ) == %d\n", result );     /* 11 */
-
-
-//     result = maximum( 3, 8, 12, 1, -1 );
-//     printf( "maximum( 3, 8, 12, 1, -1 ) == %d\n", result );  /* 12 */
-
-//     return EXIT_SUCCESS;
-
-// }
-
